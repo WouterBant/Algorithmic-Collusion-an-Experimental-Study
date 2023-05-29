eecb6e25cffa8b5d982adabd56c83ed033ced741
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-05-29 07:52:56 +0200

    5 different gamma runs

--- src/analysis_util/cylcle_classifier.py
+++ src/analysis_util/cylcle_classifier.py
@@ -9,31 +9,81 @@ class Metric(Enum):
 
 
 class Cycle_Classifier:
+    """ Class for cycle classification and analysis. """
+
     def __init__(self, pi1_L, pi2_L, theta1_L, theta2_L, q1_L, q2_L):
+        """
+        Initializes the Cycle_Classifier object.
+
+        Args:
+            pi1_L (list): List of floats.
+            pi2_L (list): List of floats.
+            theta1_L (list): List of floats.
+            theta2_L (list): List of floats.
+            q1_L (list): List of floats.
+            q2_L (list): List of floats.
+        """
         self.found_cycles = dict()
         self.create_dictionary(pi1_L, pi2_L, theta1_L, theta2_L, q1_L, q2_L)
 
     def identify_cycle(self, cur_pi1, cur_pi2, cur_theta1, cur_theta2, cur_q1, cur_q2):
+        """
+        Identifies a cycle at the end of a given episode.
+
+        Args:
+            cur_pi1 (list): List of floats.
+            cur_pi2 (list): List of floats.
+            cur_theta1 (list): List of floats.
+            cur_theta2 (list): List of floats.
+            cur_q1 (list): List of floats.
+            cur_q2 (list): List of floats.
+
+        Returns:
+            list: List of tuples representing the identified cycle.
+        """
         cycle = []
         for t in range(len(cur_pi1)-1, -1, -1):
             cur_play = (cur_pi1[t], cur_pi2[t], cur_theta1[t], cur_theta2[t], cur_q1[t], cur_q2[t])
+            # When the current state was also the first state, a cycle has been found
             if len(cycle) > 0 and cur_play == cycle[0]:
                 break
+            # Otherwise add the current state to the cycle being created
             cycle.append(cur_play)
         return cycle[::-1]
     
     def alternative_cycle(self, pattern):
+        """
+        Generates the cycle pattern, but now players are switched.
+
+        Args:
+            pattern (list): List of tuples representing a cycle.
+
+        Returns:
+            list: List of tuples representing the alternative cycle pattern.
+        """
         cycle = []
         for a, b, c, d, e, f in pattern:
             cycle.append((b, a, d, c, f, e))
         return cycle
 
     def create_dictionary(self, pi1_L, pi2_L, theta1_L, theta2_L, q1_L, q2_L):
+        """
+        Creates a dictionary of cycles and their number of occurrences.
+
+        Args:
+            pi1_L (list): List of floats.
+            pi2_L (list): List of floats.
+            theta1_L (list): List of floats.
+            theta2_L (list): List of floats.
+            q1_L (list): List of floats.
+            q2_L (list): List of floats.
+        """
         for i in range(len(q2_L)):
             cycle = self.identify_cycle(pi1_L[i], pi2_L[i], theta1_L[i], theta2_L[i], q1_L[i], q2_L[i])
             cycle_alternative = self.alternative_cycle(cycle)
             cycle_already_seen = False
 
+            # See if the current cycle - in any order - has already been seen
             for i in range(len(cycle)):
                 possible_order = tuple(cycle[i:] + cycle[:i])
                 if possible_order in self.found_cycles:
@@ -51,14 +101,32 @@ class Cycle_Classifier:
                 self.found_cycles[tuple(cycle)] = 1
 
     def number_of_cycles(self):
+        """
+        Identifies the number of different cycles found.
+        
+        Returns:
+            int: Number of different cycles found in the 1,000 episodes.
+        """
         return len(self.found_cycles)
     
     def most_found_cycle(self):
+        """
+        Identifies the most found cycle.
+
+        Returns:
+            tuple: Most found cycle and the number of times it is found.
+        """
         most_found_cycle = max(self.found_cycles, key=self.found_cycles.get)
         number_of_times_found = self.found_cycles[most_found_cycle]
         return (most_found_cycle, number_of_times_found)
     
     def longest_cycles(self):
+        """
+        Identifies the longest cycles.
+
+        Returns:
+            list: List of tuples containing the longest cycles and their number of occurrences.
+        """
         len_longest_cycles = 0
         longest_cycles = []
         for cycle in self.found_cycles.keys():
@@ -70,6 +138,12 @@ class Cycle_Classifier:
         return [(cycle, self.found_cycles[cycle]) for cycle in longest_cycles]
 
     def most_profitable_cycle(self):
+        """
+        Identifies the most profitable cycles.
+
+        Returns:
+            list: Highest profit + list of tuples containing the highest profitable cycles and their number of occurrences.
+        """
         highest_profit = 0
         highest_profitable_cycles = []
         for cycle in self.found_cycles.keys():
@@ -85,6 +159,12 @@ class Cycle_Classifier:
         return [highest_profit] + [(cycle, self.found_cycles[cycle]) for cycle in highest_profitable_cycles]
     
     def subcompetitive_profit_cycles(self):
+        """
+        Identifies all subcompetitive profit cycles.
+
+        Returns:
+            list: List of tuples containing cycles with subcompetitive profits and their number of occurrences.
+        """
         subcompetitive_cycles = []
         for cycle in self.found_cycles.keys():
             cur_profit = 0
@@ -96,6 +176,15 @@ class Cycle_Classifier:
         return [(cycle, self.found_cycles[cycle]) for cycle in subcompetitive_cycles]
     
     def mean_variance_calculator(self, metric):
+        """
+        Calculates the mean and variance of a given metric for all cycles, taking into account the number of occurrences.
+
+        Args:
+            metric (Metric): The metric to calculate mean and variance for.
+
+        Returns:
+            tuple: A tuple containing the mean and variance of the metric.
+        """
         idx = 0 if metric == Metric.Profit else 2 if metric == Metric.Theta else 4
         vals = []
         for cycle in self.found_cycles.keys():
@@ -108,10 +197,28 @@ class Cycle_Classifier:
         return (statistics.mean(vals), statistics.variance(vals))
     
     def mean_variance_profit(self):
+        """
+        Calculates the mean and variance of the profit metric for all cycles, taking into account the number of occurrences.
+
+        Returns:
+            tuple: A tuple containing the mean and variance of the profit metric.
+        """
         return self.mean_variance_calculator(Metric.Profit)
     
     def mean_variance_theta(self):
+        """
+        Calculates the mean and variance of the theta metric for all cycles, taking into account the number of occurrences.
+
+        Returns:
+            tuple: A tuple containing the mean and variance of the theta metric.
+        """
         return self.mean_variance_calculator(Metric.Theta)
     
     def mean_variance_q(self):
+        """
+        Calculates the mean and variance of the production quantity metric for all cycles, taking into account the number of occurrences.
+
+        Returns:
+            tuple: A tuple containing the mean and variance of the production quantity metric.
+        """
         return self.mean_variance_calculator(Metric.Production_quantity)
---
+++ src/analysis_util/visualize.py
@@ -0,0 +1,81 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def visualize_last_L(pi1, pi2, theta1, theta2, q1, q2):
+    plt.figure(figsize=(10, 6))
+
+    # Plot for player 1
+    plt.subplot(2, 1, 1)
+    plt.plot(pi1, label='pi1'); plt.plot(theta1, label='theta1'); plt.plot(q1, label='q1')
+    plt.legend()
+    plt.title('Variables 1'); plt.xlabel('Index'); plt.ylabel('Value')
+
+    # Plot for player 2
+    plt.subplot(2, 1, 2)
+    plt.plot(pi2, label='pi2'); plt.plot(theta2, label='theta2'); plt.plot(q2, label='q2')
+    plt.legend()
+    plt.title('Variables 2'); plt.xlabel('Index'); plt.ylabel('Value')
+
+    # Display plots
+    plt.tight_layout()
+    plt.show()
+
+
+def visualize_cycle(cycle):
+    pi1_cycle, pi2_cycle, theta1_cycle, theta2_cycle, q1_cycle, q2_cycle = [], [], [], [], [], []
+    for pi1, pi2, theta1, theta2, q1, q2 in cycle:
+        pi1_cycle.append(pi1); pi2_cycle.append(pi2)
+        theta1_cycle.append(theta1); theta2_cycle.append(theta2)
+        q1_cycle.append(q1); q2_cycle.append(q2)
+
+    plt.figure(figsize=(10, 6))
+
+    # Plot for player 1
+    plt.subplot(2, 1, 1)
+    plt.plot(pi1_cycle, label='pi1'); plt.plot(theta1_cycle, label='theta1'); plt.plot(q1_cycle, label='q1')
+    plt.legend()
+    plt.title('Variables 1'); plt.xlabel('Index'); plt.ylabel('Value')
+
+    # Plot for player 2
+    plt.subplot(2, 1, 2)
+    plt.plot(pi2_cycle, label='pi2'); plt.plot(theta2_cycle, label='theta2'); plt.plot(q2_cycle, label='q2')
+    plt.legend()
+    plt.title('Variables 2'); plt.xlabel('Index'); plt.ylabel('Value')
+
+    # Display plots
+    plt.tight_layout()
+    plt.show()
+
+
+def visualize_convergence_over_T(pi1_t, pi2_t):
+    group_size = 5
+    pi1_t_grouped = pi1_t.reshape(-1, group_size)
+    pi2_t_grouped = pi2_t.reshape(-1, group_size)
+
+    # Calculate mean and variance for each group
+    pi1_t_avg = np.mean(pi1_t_grouped, axis=1)
+    pi1_t_var = np.var(pi1_t_grouped, axis=1)
+    pi2_t_avg = np.mean(pi2_t_grouped, axis=1)
+    pi2_t_var = np.var(pi2_t_grouped, axis=1)
+
+    # Create x-axis values
+    x = np.arange(0, 500001, 50000)
+
+    # Plot mean values
+    plt.plot(x, pi1_t_avg, label='pi1_t')
+    plt.plot(x, pi2_t_avg, label='pi2_t')
+
+    # Add error bars for variance
+    plt.errorbar(x, pi1_t_avg, yerr=np.sqrt(pi1_t_var), linestyle='None', marker='o', label='Variance pi1_t')
+    plt.errorbar(x, pi2_t_avg, yerr=np.sqrt(pi2_t_var), linestyle='None', marker='o', label='Variance pi2_t')
+
+    # Add labels and title
+    plt.xlabel('X Values')
+    plt.ylabel('Averages')
+    plt.title('Averages and Variance for Grouped Data')
+
+    # Add legend
+    plt.legend()
+
+    # Display the plot
+    plt.show()
