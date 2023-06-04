fb61ad83a0436fb159cf0c0ea2f6d6e523d8b6d1
WouterBant <99251015+WouterBant@users.noreply.github.com>
2023-06-04 15:49:28 +0200

    regulator works and is able to make outcomes competitive

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,4 +1,5 @@
 from classes.action import Action
+from classes.regulator import Regulator
 from classes import Qlearning, DQN
 import numpy as np
 import os
@@ -218,6 +219,119 @@ def simulate_episode_DQN(env, Qs, Thetas, gamma=0.9, T=100_000, L=100):
 
     pi1_t, pi2_t, theta1_t, theta2_t, q1_t, q2_t = [], [], [], [], [], []
     for i in range(0, T, 9999):
+        pi1_t += pi1_ep[i:i+5]
+        pi2_t += pi2_ep[i:i+5]
+        theta1_t += theta1[i:i+5]
+        theta2_t += theta2[i:i+5]
+        q1_t += q1[i:i+5]
+        q2_t += q2[i:i+5]
+    
+    return (pi1_L, pi2_L, theta1_L, theta2_L, q1_L, q2_L,
+            pi1_t, pi2_t, theta1_t, theta2_t, q1_t, q2_t)
+
+
+def simulate_episode_with_regulator(env, Qs, Thetas, gamma=0.9, T=500_000, L=100):
+    """
+    Simulates an episode of the game with a regulator.
+
+    Args:
+        env (Environment): The economic environment.
+        Qs (list): List of allowed production quantities.
+        k (int): Parameter used to create the set of available actions (default: 6).
+        gamma (float): Discount factor (default: 0.9).
+        T (int): Number of iterations in an episode (default: 500,000).
+        L (int): Number of last runs to collect metrics from (default: 1,000).
+
+    Returns:
+        tuple: A tuple containing the metrics from the last L runs:
+            - pi1_L (list): List of profits for player 1.
+            - pi2_L (list): List of profits for player 2.
+            - theta1_L (list): List of theta values for player 1.
+            - theta2_L (list): List of theta values for player 2.
+            - q1_L (list): List of production quantities for player 1.
+            - q2_L (list): List of production quantities for player 2.
+    """
+    # Create the set of available actions
+    actions = create_actions(Qs, Thetas)
+
+    # Pick the first two prices of both players randomly
+    Action1, Action2, Action1_next, Action2_next = np.random.choice(actions, size=4)
+    
+    # Create both agents
+    Agent1 = Qlearning.Agent(actions, gamma)
+    Agent2 = Qlearning.Agent(actions, gamma)
+
+    # Create a regulator for both agents
+    Regulator1 = Regulator(env)
+    Regulator2 = Regulator(env)
+
+    # Keep track of metrics
+    pi1_ep, pi2_ep = [], []
+    q1, q2 = [], []
+    theta1, theta2 = [], []
+
+    for t in range(3, T+1):
+        if t % 2:
+            # Get action and state
+            current_action1 = Action1_next
+            current_action2 = Action2
+
+            # Compute the reward
+            reward = Regulator1.enforce_regulations(current_action1)
+
+            # Calculate the current profits
+            pi1, pi2 = env.Pi(current_action1, current_action2)
+            
+            pi1 += reward
+            pi2 += Regulator2.most_recent_reward
+
+            # Calculate the next profit
+            pi1_next = env.Pi(current_action1, Action2_next)[0] + reward
+
+            # Update Q-function
+            Agent1.learn(state=current_action2, action=current_action1, 
+                         next_state=Action2_next, profit=pi1, 
+                         next_profit=pi1_next, time=t)
+
+            # Update current action player 1, and determine next action based on the next state
+            Action1, Action1_next = Action1_next, Agent1.act(state=Action2_next, time=t)
+        else:
+            # Get state and action
+            current_action1 = Action1
+            current_action2 = Action2_next
+
+            # Compute the reward
+            reward = Regulator2.enforce_regulations(current_action2)
+
+            # Calculate the current profits
+            pi1, pi2 = env.Pi(current_action1, current_action2)
+
+            pi1 += Regulator1.most_recent_reward
+            pi2 += reward
+
+            # Calculate the next profit
+            pi2_next = env.Pi(Action1_next, current_action2)[1] + reward
+
+            # Update Q-function
+            Agent2.learn(state=current_action1, action=current_action2,
+                         next_state=Action1_next, profit=pi2,
+                         next_profit=pi2_next, time=t)
+
+            # Update current action player 1, and determine next action based on the next state
+            Action2, Action2_next = Action2_next, Agent2.act(state=Action1_next, time=t)
+        
+        # Append the profits, q's and theta's
+        pi1_ep.append(pi1); pi2_ep.append(pi2)
+        q1.append(current_action1.q); theta1.append(current_action1.theta)
+        q2.append(current_action2.q); theta2.append(current_action2.theta)
+    
+    # Get the profits, theta's, and q's from the last L runs
+    pi1_L, pi2_L = pi1_ep[-L:], pi2_ep[-L:]
+    theta1_L, theta2_L = theta1[-L:], theta2[-L:]
+    q1_L, q2_L = q1[-L:], q2[-L:]
+
+    pi1_t, pi2_t, theta1_t, theta2_t, q1_t, q2_t = [], [], [], [], [], []
+    for i in range(0, T, 49999):
         pi1_t += pi1_ep[i:i+5]
         pi2_t += pi2_ep[i:i+5]
         theta1_t += theta1[i:i+5]
--- src/classes/regulator.py
+++ src/classes/regulator.py
@@ -1,23 +1,39 @@
 from collections import deque
+from statistics import mean
 
 
 class Regulator:
     """ Regulator agent in the artificial market. """
 
-    def __init__(self, production_quota=1.8, CSR_quota=0.4, evaluation_period=5):
+    def __init__(self, env, omega=1, kappa=1, delta=1.1, production_quota=0.1, CSR_quota=0.3, evaluation_period=5):
         """
         Initializes a Regulator.
 
         Args:
             actions (np array): List of available actions.
         """
-        self.evaluation_period = evaluation_period
+        # Parameters in the penalty equation
+        self.omega = omega
+        self.kappa = kappa
+        self.delta = delta
+
+        # Thresholds for (acceptable) collusion
         self.production_quota = production_quota
         self.CSR_quota = CSR_quota
-        self.consecutive_violations = 0
-        self.theta_sum = self.q_sum = 0
-        self.thetas = deque()
-        self.qs = deque()
+
+        # Collusive and competitive thresholds
+        self.collusive_theta, self.competitive_theta = env.get_theta()
+        self.collusive_q, self.competitive_q = env.get_q()
+
+        # Used to keep track of actions taken
+        self.consecutive_violations = -1
+        self.thetas = deque(maxlen=evaluation_period)
+        self.qs = deque(maxlen=evaluation_period)
+
+        # Budget of the regulator
+        self.budget = 0
+
+        self.most_recent_reward = 0
 
     def enforce_regulations(self, action):
         """
@@ -29,22 +45,12 @@ class Regulator:
         Returns:
             float: The penalty value for violating regulations.
         """
-        if len(self.thetas) < 5:
-            self.thetas.append(action.theta)
-            self.qs.append(action.q)
-            self.theta_sum += action.theta
-            self.q_sum += action.q
-            return 0
-        out_theta, out_q = self.thetas.popleft(), self.qs.popleft()
-        self.theta_sum += action.theta - out_theta
-        self.q_sum += action.q - out_q
-        if not (penalty := self.compute_penalty()):
-            self.consecutive_violations = 0
-        else:
-            self.consecutive_violations += 1
-        return penalty
+        self.thetas.append(action.theta)
+        self.qs.append(action.q)
+        self.most_recent_reward = self.compute_reward()
+        return self.most_recent_reward
         
-    def compute_penalty(self):
+    def compute_reward(self):
         """
         Computes the penalty for violating regulations based on the accumulated theta and q values.
         Penalties increase exponentially when quotas are consecutively not met.
@@ -52,8 +58,22 @@ class Regulator:
         Returns:
             float: The penalty value.
         """
-        penalty_theta = -max(self.theta_sum/self.evaluation_period - self.CSR_quota, 0)
-        penalty_q = -max(self.production_quota - self.theta_sum/self.evaluation_period, 0)
-        multiplier = 1.1**(self.consecutive_violations)
-        penalty = (penalty_theta + penalty_q) * multiplier
-        return penalty
+        q_stand = (mean(self.qs) - self.competitive_q) / (self.collusive_q - self.competitive_q)
+        theta_stand = (mean(self.thetas) - self.competitive_theta) / (self.collusive_theta - self.competitive_theta)
+        reward_h = self.omega * (self.production_quota - q_stand) + self.kappa * (self.CSR_quota - theta_stand)
+
+        # Determine if there is a violation by the sign of reward_h
+        if reward_h >= 0:
+            self.consecutive_violations = -1
+            reward = reward_h * (self.delta ** self.consecutive_violations)
+
+            # Do not reward when there is no budget for it
+            if reward > self.budget:
+                reward = 0
+            else:
+                self.budget -= reward
+        else:
+            self.consecutive_violations += 1
+            reward = reward_h * (self.delta ** self.consecutive_violations)
+            self.budget -= reward  # Increase in budget when violation
+        return reward
